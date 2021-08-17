cd7a583502f09116247cb028b0134f1ba4dcc92d
최소희 <chosohi@naver.com>
2021-08-18 00:51:15 +0900

    modified

--- 2day/18238.py
+++ 2day/18238.py
@@ -12,11 +12,14 @@ length = len(alpha) // 2
 string = input()
 
 result = 0
-
+start = 0
 for i in range(len(string)):
     if alpha.index(string[i]) <= length:
         result +=alpha.index(string[i])
     else:
-        #역행하면 1 더 빼줘야함 혹시나~~~ ㅎㅎ 
+        #역행하면 1 더 빼줘야함 혹시나~~~ ㅎㅎ 0 
         result += abs(alpha.index(string[i]) - len(alpha) - 1)
 print(result)
+
+# 이거 어려워 ㅠㅠ 알겠습니다 도와주셔서 감사해요 재호아니 내가 언제 그 항상 우러러보죠 왜그러십니까
+#으잉?! 내가 으 약아빠지긴 ~~~~ 아뉘,,,, ㅇㄴㄹㅇㄹㄴㄴㄴㅇㄹㅇ
